32e5f569d6cb875225baf531ff11183901049e92
Anmol Tiwari <tiwariprince795@gmail.com>
2020-08-29 17:08:19 +0530

    hiii

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
-
-signUpButton.addEventListener('click',()=>{
-    container.classList.add("right-panel-active");
-});
-
-signInButton.addEventListener('click',()=>{
-    container.classList.remove("right-panel-active");
-});
+document.querySelector('.img-btn').addEventListener('click', function()
+    {
+    document.querySelector('.cont').classList.toggle('s-signup')
+    }
+);
